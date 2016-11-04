c8337cdacbab4273f05e9d532141d90ce3ba3a09
Facundo Borda <faku_kpg@hotmail.com>
2016-11-04 14:38:47 -0300

    Base CompraBono

--- ClinicaFrba/Compra Bono/ComprarBono.cs
+++ ClinicaFrba/Compra Bono/ComprarBono.cs
@@ -12,9 +12,39 @@ namespace ClinicaFrba.Compra_Bono
 {
     public partial class ComprarBono : Form
     {
+        private float precioBono;
+
         public ComprarBono()
         {
             InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            
+
+            if (MessageBox.Show("Bonos comprados con exito!!!!", "Alerta",
+                 MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                 == DialogResult.OK)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNombreAfiliado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            float montoTotal = float.Parse(numCantBonos.Value.ToString()) * precioBono;
+            lblTotalNum.Text = montoTotal.ToString();
         }
     }
 }
